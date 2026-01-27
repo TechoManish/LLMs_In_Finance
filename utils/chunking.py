1d0983fdf0e054f93c04c2dcaa921f6fea604a31
TechoManish <98860618+TechoManish@users.noreply.github.com>
2026-01-27 18:54:25 +0530

    Create chunking.py

---
+++ utils/chunking.py
@@ -0,0 +1,12 @@
+def chunk_text(text, chunk_size=120, overlap=30):
+    words = text.split()
+    chunks = []
+    start = 0
+
+    while start < len(words):
+        end = start + chunk_size
+        chunk = " ".join(words[start:end])
+        chunks.append(chunk)
+        start = end - overlap
+
+    return chunks
